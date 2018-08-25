a67ba141cbebb9d6b3cd0dec835faf1c96bbee4c
Cristian Mauricio Guerrero Chavez <guerrero.chavez.cristian@gmail.com>
2018-08-24 21:14:55 -0500

    base with modules

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    '905575796585-esm6npnet934t7dvqeamt883m2jkus25.apps.googleusercontent.com',
+  googleClientSecret: 'NdVd-LZOXlAkvuTmIXPmACNs',
+  mongoURI: 'mongodb://guerrero:FAvWewfupjalsO9@ds133152.mlab.com:33152/blog_dev_node',
+  cookieKey: '123123123'
+}
+
